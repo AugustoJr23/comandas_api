6dcdaccf9806491eb040f885d42db78dd4dd3840
Abc Bolinhas <abc@bolinhas.com>
2024-04-02 19:33:52 -0300

    Resolvido problema de exeução dos testes

--- scr/settings.py
+++ scr/settings.py
@@ -1,4 +1,4 @@
-from urllib.parse import quote # por causa do @ na senha...
+#from urllib.parse import quote # por causa do @ na senha...
 from dotenv import load_dotenv, find_dotenv
 import os
 
@@ -20,7 +20,8 @@ DB_NAME = os.getenv("DB_NAME")
 # Caso seja diferente de sqlite
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
-DB_PASS = quote(os.getenv("DB_PASS"))
+#DB_PASS = quote(os.getenv("DB_PASS"))
+DB_PASS = (os.getenv("DB_PASS"))
 
 # Ajusta STR_DATABASE conforme gerenciador escolhido
 if DB_SGDB == 'sqlite': # SQLite
